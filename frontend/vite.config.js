9355042ea528b9aeb892a9c53fb61bca3a4e3078
Kunal Udhwani <udhwanikunal@gmail.com>
2025-11-16 01:41:31 +0530

    Update Vite config to ensure _redirects is copied

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        rollupOptions: {
+            input: {
+                main: './index.html'
+            }
+        }
+    },
+    publicDir: 'public',
     server: {
         port: 5173,
         proxy: {
